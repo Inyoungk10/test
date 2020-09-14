4d7b7a7e81a0066f37eb040922734462541ebabc
Inyoung <inyoungk10@gmail.com>
2020-09-14 02:05:36 -0700

    Commit Test

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.print("Hello world");
+        System.out.println("Hello world");
+        System.out.println("commit test");
     }
 }
